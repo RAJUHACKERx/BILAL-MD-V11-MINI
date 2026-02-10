59d6b1ff04510fab70e263646933ec25cde324b5
UMAR • FAROOQ <ufprince1@gmail.com>
2026-02-10 13:04:33 +0500

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -2,17 +2,17 @@ const { cmd } = require('../command');
 const axios = require('axios');
 
 cmd({
-    pattern: "yupra",
-    alias: ["gpt5", "smartai"],
+    pattern: "ai",
+    alias: ["gpt", "gemini"],
     desc: "Chat with Yupra GPT-5 AI",
     category: "ai",
-    react: "🤖",
+    react: "☺️",
     filename: __filename
 },
 async (conn, mek, m, { q, reply, react }) => {
     try {
         if (!q) {
-            return reply("Example:\n.yupra Hello");
+            return reply("*AP KE PAS KOI SAWAL HAI 🤔 AUR APKO USKA JAWAB NAHI MIL RAHA 😢*\n*TO AP ESE LIKHO 🤗*\n\n*.AI ❮APKA SAWAL❯*\n*.GPT ❮APKA SAWAL❯*\n*.GEMINI ❮APKA SAWAL❯*\n\n*JAB AP ESE LIKHO GE 🤗 TO APKE SAWAL KA JAWAB BATA DIYA JAYE GA 😍♥️*");
         }
 
         const apiUrl = `https://api.yupra.my.id/api/ai/gpt5?text=${encodeURIComponent(q)}`;
@@ -24,16 +24,16 @@ async (conn, mek, m, { q, reply, react }) => {
         console.log("YUPRA RAW:", data);
 
         if (!data || !data.result) {
-            await react("❌");
-            return reply("AI did not return a response.");
+            await react("😔");
+            return reply("*APKE SAWAL KA JAWAB NAHI MILA SORRY 😔*.");
         }
 
-        await reply(`🤖 *GPT-5 Smart:*\n\n${data.result}`);
-        await react("✅");
+        await reply(`*👑 BILAL-MD INTELLIGENCE 👑*\n\n${data.result}`);
+        await react("😍");
 
     } catch (err) {
-        console.error("Yupra AI error:", err);
-        await react("❌");
-        reply("Failed to reach Yupra AI.");
+        console.error("*APKE SAWAL KA JAWAB NAHI MILA SORRY G 😔*:", err);
+        await react("😔");
+        reply("FAILED.....");
     }
 });
