1b2b3196c1266b191fd75425f3bd9b3affccf92d
UMAR • FAROOQ <ufprince1@gmail.com>
2026-02-12 12:37:04 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 const port = process.env.PORT || 8000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pair.html'));
+});
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
